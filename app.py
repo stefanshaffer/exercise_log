8282375d7949a9b6c81c4f05d209377d6b1e00bd
stefanshaffer <shaffer.stefan@gmail.com>
2022-08-07 21:37:37 -0400

    Added bootstrap to the layout and put the content in a container

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def home():  # put application's code here
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
