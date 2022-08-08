a500b6e1b9b1ae28285ed5914870abd96611ddde
stefanshaffer <shaffer.stefan@gmail.com>
2022-08-07 21:08:13 -0400

    Added some dummy data and displayed it on the home page

--- app.py
+++ app.py
@@ -2,11 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Stefan',
+        'title': 'Learning Python',
+        'content': 'I am learning python.  This is some content.  This is some more content.',
+        'date_posted': 'April 20th, 2022'
+    },
+    {
+        'author': 'Stefan',
+        'title': 'Learning Python 2',
+        'content': 'I am still learning python.  This is some content.  This is some more content.',
+        'date_posted': 'April, 21st, 2022'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():  # put application's code here
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
