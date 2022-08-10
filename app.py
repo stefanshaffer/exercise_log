c7414ca569debb806f87fdb1649c611f8e3a4e46
stefanshaffer <shaffer.stefan@gmail.com>
2022-08-09 20:29:43 -0400

    Added a login and register page with forms.  Set up the post request for the register page and the flash alerts.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'b8831ce3a91d35dd9088d4a2e7762df8'
 
 posts = [
     {
@@ -17,14 +20,31 @@ posts = [
     }
 ]
 
+
 @app.route('/')
 @app.route('/home')
 def home():  # put application's code here
     return render_template('home.html', posts=posts)
 
+
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
